02abacd46daa9692d76acfcd0b082562c7df3aa8
Aurgart <ilyalemeshew@gmail.com>
2026-04-15 16:57:34 +0300

    Init master

---
+++ src/main/java/java_jabki/jiro_team/JiroTeamApplication.java
@@ -0,0 +1,13 @@
+package java_jabki.jiro_team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiroTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiroTeamApplication.class, args);
+	}
+
+}
